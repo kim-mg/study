519c1e519e54f052e755e43c1321d750b06d11b4
kim-mg <audrjs3535@naver.com>
2021-09-03 19:14:18 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Customer from './components/Customer';
+
+const customers = [
+  {
+  'id': 1,
+  // 랜덤으로 이미지를 보여주는 사이트
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '조조',
+  'birthday': '940101',
+  'gender': '여자',
+  'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '941212',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '미미',
+    'birthday': '941010',
+    'gender': '여자',
+    'job': '대학생'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develpo management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+          );
+        })
+      }
     </div>
   );
 }
