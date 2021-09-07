206fb51f9d3df939834690831cd5524543515642
kim-mg <audrjs3535@naver.com>
2021-09-07 18:00:10 +0900

    Add Customer Delete Module

--- management/server.js
+++ management/server.js
@@ -25,7 +25,7 @@ const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req, res) => {
     connection.query(
-        "SELECT * FROM CUSTOMER",
+        "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
         (err, rows, fields) => {
             res.send(rows);
         }
@@ -35,7 +35,7 @@ app.get('/api/customers', (req, res) => {
 app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, NOW(), 0)';
     let image = '/image/' + req.file.filename;
     // multer 라이브러리가 겹치지 않는 이름으로 설정한다.
     let name = req.body.name;
@@ -50,5 +50,14 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
         }
     );
 });
+
+app.delete('/api/customers/:id', (req, res) => {
+    let = sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
